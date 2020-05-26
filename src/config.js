077a46107a0868c8d1bb61309beb6fe4d885529d
Leonardo Flores <tuxeww6@gmail.com>
2020-05-26 02:31:12 -0300

    apinode

---
+++ src/config.js
@@ -0,0 +1,11 @@
+global.SALT_KEY = "f5b99242-6504-4ca3-90f2-05e78e5761ef";
+global.EMAIL_TMPL = "Olá, <strong>{0}</strong>, seja bem vindo à Node Store!";
+
+module.exports = {
+  connectionString:
+    "mongodb+srv://admin:admin@node-str-b4jmn.azure.mongodb.net/test?retryWrites=true&w=majority",
+  sendgridKey:
+    "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",
+  containerConnectionString:
+    "DefaultEndpointsProtocol=https;AccountName=nodeapistore;AccountKey=j9f4wKy114lUoeUsLFoGyXXU6CQOrBrOcQrMGmpS/pweu8Y0hm+px15AJRmSscROYPQqvPJvN22/F/M6DTmCQw==;EndpointSuffix=core.windows.netDefaultEndpointsProtocol=https;AccountName=nodeapistore;AccountKey=j9f4wKy114lUoeUsLFoGyXXU6CQOrBrOcQrMGmpS/pweu8Y0hm+px15AJRmSscROYPQqvPJvN22/F/M6DTmCQw==;EndpointSuffix=core.windows.net",
+};
